5c251ca6e87f17927a38666889e346133042a1b5
Rohit <Rohitkale907@gmail.com>
2022-07-29 15:43:48 +0530

    made all correction

--- src/main/java/com/springBoot/Rest/Service/BookServices.java
+++ src/main/java/com/springBoot/Rest/Service/BookServices.java
@@ -1,10 +1,6 @@
 package com.springBoot.Rest.Services;
 
-
-
-
 import java.util.List;
-
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.CacheEvict;
@@ -15,53 +11,51 @@ import org.springframework.stereotype.Service;
 import com.springBoot.Rest.Dao.BookRepository;
 import com.springBoot.Rest.Model.Book;
 
-
-
 @Service
 public class BookServices {
 	@Autowired
 	private BookRepository bookRepository;
-    @Cacheable(value  = "books" )
-	public List<Book> getAllBooks(){
-	List<Book> list=	(List<Book>) this.bookRepository.findAll();
+
+	@Cacheable(value = "books")
+	public List<Book> getAllBooks() {
+		List<Book> list = (List<Book>) this.bookRepository.findAll();
 		return list;
-		
+
 	}
-    
-    @Cacheable(cacheNames = "books" ,key = "#id")
+
+	@Cacheable(cacheNames = "books", key = "#id")
 	public Book getBookById(int id) {
-	Book book =null;
-	try {
-//		book=	list.stream().filter(e->e.getId()==id).findFirst().get();
-		
-    book=this.bookRepository.findById(id);
-		
-	} catch (Exception e2) {
-		e2.printStackTrace();
+		Book book = null;
+		try {
+			// book= list.stream().filter(e->e.getId()==id).findFirst().get();
+
+			book = this.bookRepository.findById(id);
+
+		} catch (Exception e2) {
+			e2.printStackTrace();
+		}
+		return book;
 	}
-	return book;
-	}
-	
 
 	public Book addBook(Book b) {
-	Book result =	bookRepository.save(b);
+		Book result = bookRepository.save(b);
 
 		return result;
-		
+
 	}
-	@CacheEvict(value  = "books" , key = "#id")
+
+	@CacheEvict(value = "books", key = "#id")
 	public void deleteBookById(int id) {
-//		list.stream().filter(e->e.getId()==id).collect(Collectors.toList());
+		// list.stream().filter(e->e.getId()==id).collect(Collectors.toList());
 		bookRepository.deleteById(id);
-		
+
 	}
-	@CachePut(cacheNames = "books" , key = "#id")
-	public void updateBook(Book book , int id) {
-		
+
+	@CachePut(cacheNames = "books", key = "#id")
+	public void updateBook(Book book, int id) {
+
 		book.setId(id);
 		bookRepository.save(book);
 	}
-		
-		
-		
+
 }
